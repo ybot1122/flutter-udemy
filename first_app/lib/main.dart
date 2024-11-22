d4d58018f25d8ff99a7308be285e6855ab951d25
Toby Liu <toby.liu@hulu.com>
2024-11-21 17:45:43 -0800

    Writing app from scratch

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,37 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("Hello World Again!!"),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World! Again',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+    ),
+  ));
 }
