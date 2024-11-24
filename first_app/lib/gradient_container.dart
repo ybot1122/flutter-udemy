c6523f1759bcdadd9c0f4ee4c949c991e08d865d
Toby Liu <toby.liu@hulu.com>
2024-11-23 19:17:39 -0800

    variables and const

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import './hello_world_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -10,8 +13,8 @@ class GradientContainer extends StatelessWidget {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
             colors: [Colors.blueGrey, Colors.orangeAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
+            begin: startAlignment,
+            end: endAlignment),
       ),
       child: const Center(
         child: HelloWorldText(),
