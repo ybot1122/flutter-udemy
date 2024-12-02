667775588900d8270cd95bbdb9d2578114b46a2f
Toby Liu <toby.liu@hulu.com>
2024-12-02 11:58:33 -0800

    widget challenge

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -6,13 +6,31 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Image.asset("assets/images/quiz-logo.png", width: 300),
-        const Text(
-          "Learn Flutter the Fun Way!",
-          style: TextStyle(),
-        )
-      ]),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            "Learn Flutter the Fun Way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text("Start Quiz"),
+          )
+        ],
+      ),
     );
   }
 }
