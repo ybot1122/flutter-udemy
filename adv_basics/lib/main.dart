c7b69a5d747e496624aa626f82e5bb44932d060d
Toby Liu <toby.liu@hulu.com>
2024-12-03 10:59:45 -0800

    stateful widget, lifting state up

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,18 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 82, 41, 153),
-            Color.fromARGB(255, 125, 71, 218)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
---
+++ adv_basics/lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:adv_basics/questions_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 82, 41, 153),
+              Color.fromARGB(255, 125, 71, 218)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
