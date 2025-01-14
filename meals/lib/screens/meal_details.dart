18d730f5076b9dc13d30b53f642febdedead7bd5
Toby Liu <ybot1122@gmail.com>
2025-01-14 10:12:50 -0800

    provider for favorites icon

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -10,6 +10,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
         appBar: AppBar(
           title: Text(meal.title),
@@ -29,7 +33,7 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(Icons.star),
+              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
             )
           ],
         ),
