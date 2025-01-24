0086d4ff2e350817cda9d634f60a20921afb1103
Toby Liu <ybot1122@gmail.com>
2025-01-24 13:16:02 -0800

    Getting started with the favorite_places app

---
+++ favorite_places/lib/screens/PlaceDetails.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Details about a place'),
+    );
+  }
+}
---
+++ favorite_places/lib/screens/Places.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('I am places screen'),
+    );
+  }
+}
